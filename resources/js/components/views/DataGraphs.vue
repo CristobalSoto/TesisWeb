bd8e2ad2f9b9e74a402ac8f4a165d1e5b4c31fdd
CristobalSoto <cristobal@onesla.com>
2019-03-25 23:23:15 -0300

    update

--- resources/js/components/views/DataGraphs.vue
+++ resources/js/components/views/DataGraphs.vue
@@ -11,7 +11,7 @@
         <h5 class="card-title ml-1">Variación productos en el tiempo</h5>
       </div>
       <div class="col-md-6 mt-4">
-        <el-select v-model="tipoProducto" placeholder="Producto">
+        <el-select v-model="tipoProducto" placeholder="Productos">
           <el-option 
             v-for="item in options.tipoProducto" 
             :key="item.nombre" 
@@ -31,7 +31,46 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <button class="btn btn-primary" @click="getData">Cargar datos</button>
+        <button class="btn btn-primary btn-sm ml-2" @click="getDataProductsTime">Cargar datos</button>
+        <p class="text-danger ml-2 mt-2" v-if="errorProductos" >Debe ingresar el tipo de producto</p>
+        <br><br>
+        <ve-line
+          :data="chartDataProductos"
+          v-loading="isLoading"
+          :data-empty="dataEmpty"
+          >
+        </ve-line>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="row">
+      <div class="col-md-6 mt-3">
+        <h5 class="card-title ml-1">Valores de producots por región</h5>
+      </div>
+      <div class="col-md-6 mt-4">
+        <el-select v-model="tipoProducto" placeholder="Productos">
+          <el-option 
+            v-for="item in options.tipoProducto" 
+            :key="item.nombre" 
+            :label="item.nombre" 
+            :value="item.nombre">
+          </el-option>
+        </el-select>
+        <el-select v-model="medidaTiempo" placeholder="Tiempo">
+          <el-option 
+          v-for="item in options.tiempo" 
+          :key="item.metric" 
+          :label="item.metric" 
+          :value="item.metric">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <button class="btn btn-primary btn-sm ml-2" @click="getDataProductsTime">Cargar datos</button>
+        <p class="text-danger ml-2 mt-2" v-if="errorProductos" >Debe ingresar el tipo de producto</p>
         <br><br>
         <ve-line
           :data="chartData"
@@ -79,23 +118,33 @@
         },
         tipoProducto: null,
         medidaTiempo: 'Mensual',
-        chartData: {
+        chartDataProductos: {
+          columns: [],
+          rows: []
+        },
+        chartDataProductosRegion: {
           columns: [],
           rows: []
         },
         isLoading: false,
         dataEmpty: false,
+        errorProductos: false,
         meses: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
         productos : []
       }
     },
     methods: {
-      getData () {
-      
+      getDataProductsTime () {
+        if (this.tipoProducto == null) {
+          this.errorProductos = true
+          return false
+        } else {
+          this.errorProductos = false
+        }
         let index = 0
         this.isLoading = true
         
-        this.$http.get('api/productos-mensuales/' + this.tipoProducto + '/2010')
+        this.$http.get('api/productos-mensuales/' + this.tipoProducto + '/2012')
         .then(response => {
           response.body.forEach(obj => {
             
@@ -107,7 +156,7 @@
           
           this.formatData()
           //console.log(this.rawData)
-          this.chartData = this.formatedData
+          this.chartDataProductos = this.formatedData
           this.isLoading = false
         })
       },
